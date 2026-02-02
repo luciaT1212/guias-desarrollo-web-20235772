029303821adbf4d7091a7ca45501fc9dd6f00902
luciaT1212 <luciaturcios1212@gmail.com>
2026-02-01 20:40:34 -0600

    Add files via upload

---
+++ GUIA2-CARRITO-REACT-20235772/src/components/Header.jsx
@@ -0,0 +1,96 @@
+export const Header = ({ cart, total, increase, decrease, remove, vaciarCart }) => {
+  return (
+    <header className="py-5 header">
+      <div className="container-xl">
+        <div className="row justify-content-center justify-content-md-between">
+          <div className="col-8 col-md-3">
+            <a href="/">
+              <img className="img-fluid" src="/img/logo.svg" alt="logo" />
+            </a>
+          </div>
+
+          <nav className="col-md-6 mt-5 d-flex align-items-start justify-content-end">
+            <div className="carrito">
+              <img className="img-fluid" src="/img/carrito.png" alt="carrito" />
+
+              <div id="carrito" className="bg-white p-3">
+                {cart.length === 0 ? (
+                  <p className="text-center">El carrito está vacío</p>
+                ) : (
+                  <>
+                    <table className="w-100 table">
+                      <thead>
+                        <tr>
+                          <th>Imagen</th>
+                          <th>Nombre</th>
+                          <th>Precio</th>
+                          <th>Cantidad</th>
+                          <th></th>
+                        </tr>
+                      </thead>
+
+                      <tbody>
+                        {cart.map((guitar) => (
+                          <tr key={guitar.id}>
+                            <td>
+                              <img
+                                className="img-fluid"
+                                src={`/img/${guitar.image}.jpg`}
+                                alt={guitar.name}
+                              />
+                            </td>
+                            <td>{guitar.name}</td>
+                            <td className="fw-bold">${guitar.price}</td>
+                            <td className="d-flex align-items-start gap-4">
+                              <button
+                                type="button"
+                                className="btn btn-dark"
+                                onClick={() => decrease(guitar.id)}
+                              >
+                                -
+                              </button>
+
+                              {guitar.quantity}
+
+                              <button
+                                type="button"
+                                className="btn btn-dark"
+                                onClick={() => increase(guitar.id)}
+                              >
+                                +
+                              </button>
+                            </td>
+                            <td>
+                              <button
+                                className="btn btn-danger"
+                                type="button"
+                                onClick={() => remove(guitar.id)}
+                              >
+                                X
+                              </button>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+
+                    <p className="text-end">
+                      Total pagar: <span className="fw-bold">${total}</span>
+                    </p>
+
+                    <button
+                      className="btn btn-dark w-100 mt-3 p-2"
+                      onClick={vaciarCart}
+                    >
+                      Vaciar Carrito
+                    </button>
+                  </>
+                )}
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
