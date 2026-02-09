5728181d860fd737525e5a62614cd2c905c5e0c2
luciaT1212 <luciaturcios1212@gmail.com>
2026-02-08 22:37:18 -0600

    Fix Vite base for GitHub Pages (Guia 3)

--- GUIA3-HOOKS-REACT-20235772/vite.config.js
+++ GUIA3-HOOKS-REACT-20235772/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/guias-desarrollo-web-20235772/GUIA3-HOOKS-REACT-20235772/",
+  base: "./",
 });
