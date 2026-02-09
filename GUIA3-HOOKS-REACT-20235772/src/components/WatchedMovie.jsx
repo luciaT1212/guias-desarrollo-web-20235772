af5c502f55f332fc2cf70527947c9639325ed36a
luciaT1212 <luciaturcios1212@gmail.com>
2026-02-08 21:47:14 -0600

    Build guía 3

--- GUIA3-HOOKS-REACT-20235772/src/components/WatchedMovie.jsx
+++ GUIA3-HOOKS-REACT-20235772/src/components/WatchedMovie.jsx
@@ -1,14 +1,10 @@
-export function WatchedMoviesContainer({ children }) {
-  return <>{children}</>;
-}
-
-export function WatchedMoviesList({ watched, onDeleteWatched }) {
+export function WatchedMovieList({ watched, onDeleteWatched }) {
   return (
     <ul className="list">
       {watched.map((movie) => (
         <WatchedMovie
-          movie={movie}
           key={movie.imdbID}
+          movie={movie}
           onDeleteWatched={onDeleteWatched}
         />
       ))}
@@ -27,63 +23,22 @@ function WatchedMovie({ movie, onDeleteWatched }) {
           <span>⭐️</span>
           <span>{movie.imdbRating}</span>
         </p>
-
         <p>
           <span>🌟</span>
           <span>{movie.userRating}</span>
         </p>
-
         <p>
           <span>⏳</span>
           <span>{movie.runtime} min</span>
         </p>
-
-        <button
-          className="btn-delete"
-          onClick={() => onDeleteWatched(movie.imdbID)}
-        >
-          X
-        </button>
       </div>
+
+      <button
+        className="btn-delete"
+        onClick={() => onDeleteWatched(movie.imdbID)}
+      >
+        ❌
+      </button>
     </li>
-  );
-}
-
-const average = (arr) =>
-  arr.length === 0
-    ? 0
-    : arr.reduce((acc, cur) => acc + cur, 0) / arr.length;
-
-export function WatchedSummary({ watched }) {
-  const avgImdbRating = average(watched.map((m) => m.imdbRating));
-  const avgUserRating = average(watched.map((m) => m.userRating));
-  const avgRuntime = average(watched.map((m) => m.runtime));
-
-  return (
-    <div className="summary">
-      <h2>Peliculas que has visto</h2>
-
-      <div>
-        <p>
-          <span>#️⃣</span>
-          <span>{watched.length} peliculas</span>
-        </p>
-
-        <p>
-          <span>⭐️</span>
-          <span>{avgImdbRating.toFixed(2)}</span>
-        </p>
-
-        <p>
-          <span>🌟</span>
-          <span>{avgUserRating.toFixed(2)}</span>
-        </p>
-
-        <p>
-          <span>⏳</span>
-          <span>{avgRuntime.toFixed(0)} min</span>
-        </p>
-      </div>
-    </div>
   );
 }
